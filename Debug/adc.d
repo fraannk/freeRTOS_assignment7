15fcc2349c36a6663652c71cad9b461fe8ffc241
Søren Frank <sfraank@me.com>
2017-04-25 15:38:34 +0200

    more changes

---
+++ Debug/adc.d
@@ -0,0 +1,7 @@
+adc.o: ../adc.c \
+ /Users/fraannk/Documents/SDU/Elektronik-\ og\ datateknik/4.\ Semester/Embedded\ programming/Include/tm4c123gh6pm.h \
+ ../adc.h
+
+/Users/fraannk/Documents/SDU/Elektronik-\ og\ datateknik/4.\ Semester/Embedded\ programming/Include/tm4c123gh6pm.h:
+
+../adc.h:
